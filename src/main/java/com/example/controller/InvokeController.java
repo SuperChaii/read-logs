477e7d1e68a3722be688ee9cdc82e68f764960f8
chaiyibo <7871769@qq.com>
2019-02-15 14:25:48 +0800

    init

--- src/main/java/com/example/controller/InvokeController.java
+++ src/main/java/com/example/controller/InvokeController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import com.example.service.InvokeService;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,17 +20,26 @@ public class InvokeController {
     @Autowired
     InvokeService service;
 
+    /**
+     * 功能描述:
+     * 指定fileName读指定文件，否则读dir目录下全部日志
+     * fileName > 文件名
+     * dirName > log下的目录名
+     * Date:2019-02-15 > Author:Chaiyibo
+     **/
     @RequestMapping(value = "/invoke", method = RequestMethod.POST)
-    public void invoke(@RequestParam(value = "fileName") String fileName, @RequestParam(value = "dirName") String dirName, HttpServletResponse response) {
+    public void invoke(@RequestParam(value = "fileName", required = false) String fileName, @RequestParam(value = "dirName") String dirName, HttpServletResponse response) {
         String root = System.getProperty("user.dir");
         String baseFilePath = root + File.separator + "src\\main\\resources\\logs\\" + dirName;
-        //ArrayList<String> listFileName = new ArrayList();
-//        getAllFileName(baseFilePath, listFileName);
-//        for (String name : listFileName) {
-//            System.out.println(name);
-//        }
-        //String filePath = root + File.separator + "src\\main\\resources\\logs\\" + num + File.separator + fileName;
-        //service.read2fileNameList(listFileName, baseFilePath, response);
-        service.read2fileName(fileName, baseFilePath, response);
+        if (Strings.isNotBlank(fileName)) {
+            //指定文件名，则读一个文件
+            service.read2fileName(fileName, baseFilePath, response);
+        } else {
+            //未指定文件，则读目录下所有文件
+            ArrayList<String> listFileName = new ArrayList();
+            getAllFileName(baseFilePath, listFileName);
+            service.read2fileNameList(listFileName, baseFilePath, response);
+        }
+
     }
 }
